23fcbf1a61a87c942fc0a8095c2def5c8fe5ad17
AlineCoatanoan <aline.coatanoan@gmail.com>
2025-02-03 15:59:26 +0100

    delete scroll

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,55 +1,21 @@
-import { useEffect } from "react";
-import Header from "../Header";
-import Accueil from "../Accueil";
-import Profile from "../Profile";
-import Skills from "../Skills";
-import Portfolio from "../Portfolio";
-import Contact from "../Contact";
-import Footer from "../Footer";
+
+import Header from '../Header';
+import Accueil from '../Accueil';
+import Profile from '../Profile';
+import Skills from '../Skills';
+import Portfolio from '../Portfolio';
+import Contact from '../Contact';
+import Footer from '../Footer';
 
 const App = () => {
-  const handleScroll = () => {
-    const sections = document.querySelectorAll('section');
-    
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-
-      // Si la section est visible dans la fenêtre (viewport)
-      if (sectionTop < windowHeight * 0.75) {
-        section.classList.add('fade-in');
-      } else {
-        section.classList.remove('fade-in');
-      }
-    });
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <div>
       <Header />
-      <section id="accueil">
-        <Accueil />
-      </section>
-      <section id="profile">
-        <Profile />
-      </section>
-      <section id="skills">
-        <Skills />
-      </section>
-      <section id="portfolio">
-        <Portfolio />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
+      <Accueil />
+      <Profile />
+      <Skills />
+      <Portfolio />
+      <Contact />
       <Footer />
     </div>
   );
