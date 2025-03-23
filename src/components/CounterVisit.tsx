2a710c7a983e30ea77b98b6bd6f83f5d1b0f1316
AlineCoatanoan <aline.coatanoan@gmail.com>
2025-03-23 18:40:29 +0100

    update CounterVisit

--- src/components/CounterVisit.tsx
+++ src/components/CounterVisit.tsx
@@ -9,22 +9,23 @@ const VisitCounter: React.FC = () => {
 
     // Si un compteur existe déjà dans localStorage, l'utiliser
     if (storedCount) {
-      setVisitCount(Number(storedCount));
+      setVisitCount(Number(storedCount)); // Utilise le compteur déjà existant
     } else {
       // Si aucun compteur n'existe, initialiser à 1 et l'enregistrer
       setVisitCount(1);
       localStorage.setItem('visitCount', '1');
     }
-  }, []); // Ce useEffect ne s'exécute qu'une seule fois au premier rendu.
+  }, []); // Ce useEffect s'exécute uniquement lors du premier chargement
 
+  // Mettre à jour le compteur à chaque visite
   useEffect(() => {
-    // Incrémente le compteur à chaque visite
+    // Incrémente le compteur à chaque visite (si plus de 1)
     if (visitCount > 0) {
       const newCount = visitCount + 1;
-      setVisitCount(newCount);
       localStorage.setItem('visitCount', newCount.toString()); // Enregistre dans localStorage
+      setVisitCount(newCount); // Met à jour l'état local
     }
-  }, [visitCount]); // Ce useEffect se déclenche chaque fois que visitCount change.
+  }, [visitCount]); // Ce useEffect s'exécute chaque fois que le compteur change
 
   return (
     <div style={{ color: '#1D232A' }}>
