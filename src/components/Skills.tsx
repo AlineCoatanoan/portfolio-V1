65ddf4e10a58f3a0ee011d0e1280e6c197bb553c
AlineCoatanoan <aline.coatanoan@gmail.com>
2025-04-04 20:40:13 +0200

    remise à niveau skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,55 +2,44 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const Skills: React.FC = () => {
-  const softSkills = [    
-    "<span class='text-[#FDA232]'>l'empathie</span> 🤝 pour comprendre les besoins individuels,",
-    
-    "<span class='text-[#FDA232]'>l'adaptabilité</span> 🔄 face aux situations urgentes,",
-    
-    "<span class='text-[#FDA232]'>communication efficace</span> 💬 et <span class='text-[#FDA232]'>remise en question</span> pour interagir avec les personnes accompagnées mais aussi avec mes collègues, et ainsi permettre un travail en <span class='text-[#FDA232]'>collaboration avec l’équipe</span> 👥 cela m'a permis de renforcer ma capacité à travailler ensemble pour atteindre des objectifs communs.",
-    
-    "Faire preuve de <span class='text-[#FDA232]'>patience, et de persévérance</span> 💪 étaient des compétences primordiales dans le secteur social. Les personnes ont des problématiques importantes qui demandent du temps et une réflexion pour trouver des solutions.",
-    
-    "Avoir une <span class='text-[#FDA232]'>organisation 📅 rigoureuse et être consciencieuse</span> 🧐 m'ont aidée à gérer efficacement mon temps et mes priorités. Dans l’éducation spécialisée, il faut jongler entre urgence, rédaction de rapport, accompagnement au quotidien, rendez-vous, réunion...",
-    
-    "Et un <span class='text-[#FDA232]'>sens de l'humour</span> et une <span class='text-[#FDA232]'>positivité</span> 😄 m'ont permis de maintenir une ambiance agréable même dans les moments les plus difficiles."
+  const softSkills = [
+    "<span class='text-[#FDA232] font-bold text-xl'>l'empathie</span> 🤝 pour comprendre les besoins individuels,",
+    "<span class='text-[#FDA232] font-bold text-xl'>l'adaptabilité</span> 🔄 face aux situations urgentes,",
+    "<span class='text-[#FDA232] font-bold text-xl'>la communication</span> 💬 et la <span class='text-[#FDA232] font-bold text-xl'>remise en question</span> pour interagir avec les personnes accompagnées mais aussi avec mes collègues, et ainsi permettre un travail en <span class='text-[#FDA232] font-bold text-xl'>collaboration avec l’équipe</span> 👥",
+    "Faire preuve de <span class='text-[#FDA232] font-bold text-xl'>patience, et de persévérance</span> 💪 étaient des compétences primordiales dans le secteur social. Les personnes ont des problématiques importantes qui demandent du temps et une réflexion pour trouver des solutions.",
+    "Avoir une <span class='text-[#FDA232] font-bold text-xl'>organisation 📅 rigoureuse et être consciencieuse</span> 🧐 m'ont aidée à gérer efficacement mon temps et mes priorités. Dans l’éducation spécialisée, il faut jongler entre urgence, rédaction de rapport, accompagnement au quotidien, rendez-vous, réunion...",
+    "Et un <span class='text-[#FDA232] font-bold text-xl'>sens de l'humour</span> et une <span class='text-[#FDA232] font-bold text-xl'>positivité</span> 😄 m'ont permis de maintenir une ambiance agréable même dans les moments les plus difficiles."
   ];
 
   const hardSkills = {
     "Langages de Programmation": ["JavaScript 🟨", "TypeScript 🔷"],
-    "Frameworks et Bibliothèques": [
-      "React.js ⚛️",
-      "Node.js 🌱",
-      "Express.js 🚀",
-      "Next.js 🔜",
-      "Framer Motion 🎬",
-      "GSAP 🌀"
-    ],
-    "Développement Front-End": [
-      "Tailwind CSS 🎨",
-      "SEO 🔍",
-      "Responsive Design 📲",
-      "Accessibilité ♿"
+    "Bases de Données": [
+      "PostgreSQL 🗃️",
+      "MCD / MLD / MPD 🗺️"
     ],
     "Développement Back-End": [
-      "RESTful API Design 🌐",
+      "Node.js 🌱",
+      "Express.js 🚀",
       "Authentification (JWT) 🔑",
       "Joi ✅",
       "Bcrypt 🔒",
       "ORM Sequelize 🔧"
     ],
-    "Bases de Données": [
-      "MySQL, PostgreSQL 🗃️",
-      "NoSQL (Cosmos) 🌌",
-      "Modélisation MCD / MLD / MPD 🗺️"
+    "Développement Front-End": [
+      "React.js ⚛️",
+      "Next.js 🔜",
+      "Tailwind CSS 🎨",
+      "Framer Motion 🎬",
+      "GSAP 🌀",
+      "SEO 🔍",
+      "Responsive Design 📲",
+      "Accessibilité ♿"
     ],
-    "Outils de Versioning": ["Git 🧰", "GitHub 🐙"],
-    "DevOps": ["Docker 🐳", "Vercel 🌍", "Azure ☁️"],
+    
+    "Outils de Versioning": ["Git 🧰"],
     "Outils de Tests": ["Jest 🧪", "ThunderClient ⚡", "Supertest 🧑‍💻"],
-    "Méthodologies Agiles": ["Kanban - GitHub Projects 📋", "User Stories 📝"],
+    "Méthodologies Agiles": ["Kanban📋", "User Stories 📝"],
     "Outils de Design": ["Canva 🖌️", "Wireframe 🖥️", "Maquettes 🏗️"],
-    "Services": ["Microsoft Azure 🖥️"],
-    "Conception API": ["Routes API 🔀", "Dictionnaire des données 📚"]
   };
 
   return (
@@ -66,7 +55,8 @@ const Skills: React.FC = () => {
             Mes compétences
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 p-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-1">
+            {/* Hard Skills Card */}
             <motion.div
               className="flex flex-col items-center justify-between p-6 bg-[#0C3E45] rounded-lg hover:shadow-2xl transition-all sm:col-span-2 lg:col-span-1"
               initial={{ y: -50, opacity: 0 }}
@@ -75,7 +65,7 @@ const Skills: React.FC = () => {
               transition={{ duration: 0.8, delay: 0.2 }}
             >
               <motion.div
-                className="text-5xl pb-12 font-special text-center"
+                className="text-5xl font-special text-center"
                 animate={{
                   rotate: [0, 2, -2, 0],
                 }}
@@ -87,13 +77,17 @@ const Skills: React.FC = () => {
                 Hard Skills
               </motion.div>
 
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-20 mt-16 mb-8 flex-grow">
                 {Object.entries(hardSkills).map(([category, skills], index) => (
-                  <div key={index} className="flex flex-col space-y-2">
-                    <h3 className="text-xl text-[#FDA232] leading-tight">{category}</h3>
-                    <ul className="text-xl text-left list-disc list-inside">
+                  <div key={index} className="flex flex-col space-y-4">
+                    <h3 className="text-xl font-bold text-[#FDA232]">{category}</h3>
+                    <ul className="text-l text-left list-disc list-inside list-[circle]">
                       {skills.map((skill, skillIndex) => (
-                        <motion.li key={skillIndex} whileHover={{ scale: 1.2, x: 10 }}>
+                        <motion.li
+                          key={skillIndex}
+                          whileHover={{ scale: 1.2, x: 10 }}
+                          className="hover:text-[#FDA232]"
+                        >
                           {skill}
                         </motion.li>
                       ))}
@@ -103,15 +97,16 @@ const Skills: React.FC = () => {
               </div>
             </motion.div>
 
+            {/* Soft Skills Card */}
             <motion.div
-              className="flex flex-col items-center justify-between p-6 bg-[#1F595A] rounded-lg hover:shadow-2xl transition-all sm:col-span-2 lg:col-span-1 mt-0"
+              className="flex flex-col items-center justify-between p-6 bg-[#1F595A] rounded-lg hover:shadow-2xl transition-all sm:col-span-2 lg:col-span-1"
               initial={{ y: 50, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               whileHover={{ scale: 1.05 }}
               transition={{ duration: 0.8, delay: 0.4 }}
             >
-            <motion.div
-                className="text-5xl font-special mb-4 text-center"  
+              <motion.div
+                className="text-5xl font-special mb-0 text-center"
                 animate={{
                   rotate: [0, -2, 2, 0],
                 }}
@@ -124,21 +119,21 @@ const Skills: React.FC = () => {
               </motion.div>
 
               <motion.div
-  className="flex flex-col items-start justify-start text-xl text-left mb-0 mt-0 flex-grow-0"
-  >
-    <p className="mb-0 mt-0 text-xl">
-      En tant qu'éducatrice spécialisée, j'ai développé des compétences qui sauront se montrer utiles dans le développement web.
-    </p>
-                            </motion.div>
-                            <ul className="text-xl text-left list-disc list-inside space-y-2 mt-0 mb-0">
-    {softSkills.map((skill, index) => (
-      <motion.li key={index} whileHover={{ scale: 1.2, x: -10 }}>
-        <span dangerouslySetInnerHTML={{ __html: skill }} />
-      </motion.li>
-    ))}
-  </ul>
+                className="text-xl text-left mt-16 mb-8 flex-grow"
+              >
+                <p className="text-xl mb-0 text-center">
+                  En tant qu'éducatrice spécialisée, j'ai développé des compétences qui sauront se montrer utiles dans le développement web.
+                </p>
+              </motion.div>
+
+              <ul className="text-l text-left list-disc list-inside space-y-16 mt-0 list-[circle]">
+                {softSkills.map((skill, index) => (
+                  <motion.li key={index} whileHover={{ scale: 1.2, x: -10 }} >
+                    <span dangerouslySetInnerHTML={{ __html: skill }} />
+                  </motion.li>
+                ))}
+              </ul>
             </motion.div>
-            
           </div>
         </motion.div>
       </section>
